6d648e62f8ffd0bcc0e8c5dc2beb4be8071fbe9f
oisdk <mail@doisinkidney.com>
2021-08-28 20:59:08 +0100

    Update TestHelpers.hs

--- MonusWeightedSearch/Internal/TestHelpers.hs
+++ MonusWeightedSearch/Internal/TestHelpers.hs
@@ -6,6 +6,9 @@ import Test.QuickCheck
 import Numeric.Natural
 import System.Random
 
+-- | @'sumsTo' n@ generates a list that sums to @n@.
+--
+-- prop> n >= 0 ==> forAll (sumsTo n) (\xs -> sum xs === n)
 sumsTo :: Int -> Gen [Int]
 sumsTo n = go [] n >>= shuffle
   where
@@ -19,13 +22,13 @@ instance Arbitrary Natural where
   arbitrary = arbitrarySizedNatural
   shrink = map fromInteger . filter (0<=) . shrink . toInteger
 
--- | percentageChance n is True n% of the time, and false the rest of the
--- time
+-- | @'percentageChance' n@ is 'True' @n@% of the time, and 'False' the rest
+-- of the time.
 percentageChance :: Word -> Gen Bool
 percentageChance n = fmap (n>) (choose (0,99))
 
--- | percentageChanceIO n is True n% of the time, and false the rest of the
--- time
+-- | @'percentageChanceIO' n@ is 'True' @n@% of the time, and 'False' the rest
+-- of the time.
 percentageChanceIO :: Word -> IO Bool
 percentageChanceIO n = fmap (n>) (randomRIO (0,99))
 {-# INLINE percentageChanceIO #-}
