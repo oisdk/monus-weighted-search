cfeb07db8fb60dbf42da28401ac1f486bbe1a1cc
oisdk <mail@doisinkidney.com>
2021-08-28 17:01:33 +0100

    Update Dist.hs

--- Data/Monus/Dist.hs
+++ Data/Monus/Dist.hs
@@ -9,11 +9,14 @@ import Data.Data
 import GHC.Generics
 import Data.Monoid
 import Data.Monus
-
+import Test.QuickCheck
 import Control.DeepSeq
 
 newtype Dist = Dist { runDist :: Natural }
   deriving stock (Eq, Ord, Data, Generic, Typeable)
   deriving (Num, Enum, Integral, Show, Read, Real, Ix, Bits, NFData) via Natural
   deriving (Semigroup, Monoid, Monus) via (Sum Natural)
-  
+
+instance Arbitrary Dist where
+  arbitrary = arbitrarySizedNatural
+  shrink = shrinkIntegral
