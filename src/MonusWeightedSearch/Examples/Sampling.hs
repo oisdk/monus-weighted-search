7983a4a2f31c87e94a3aa81e838f48fecee3c027
oisdk <mail@doisinkidney.com>
2021-08-29 15:01:44 +0100

    Update Sampling.hs

--- src/MonusWeightedSearch/Examples/Sampling.hs
+++ src/MonusWeightedSearch/Examples/Sampling.hs
@@ -1,3 +1,15 @@
+-- |
+-- Module      : MonusWeightedSearch.Examples.Sampling
+-- Copyright   : (c) Donnacha Oisín Kidney 2021
+-- Maintainer  : mail@doisinkidney.com
+-- Stability   : experimental
+-- Portability : non-portable
+--
+-- Random sampling from the 'Heap' monad.
+--
+-- The 'Heap' monad can function as a probability monad, and it implements an
+-- efficient sampling algorithm, based on reservoir sampling.
+
 module MonusWeightedSearch.Examples.Sampling where
 
 import Control.Monad.Heap
@@ -5,6 +17,7 @@ import Data.Monus.Prob
 import Data.Ratio
 import System.Random
 
+-- | Sample a single value from the heap.
 sample :: Heap Prob a -> IO a
 sample = go 1 . search where
   go r ((x,Prob px):xs) = do
