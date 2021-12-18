ca85895ae59727e35324e43c18cc613eedea0836
oisdk <mail@doisinkidney.com>
2021-12-18 16:46:03 +0000

    Update Heap.hs

--- src/Control/Comonad/Heap.hs
+++ src/Control/Comonad/Heap.hs
@@ -39,6 +39,9 @@ import Data.Functor.Identity
 import Control.Monad (ap, join)
 import Control.Comonad
 import Control.Comonad.Cofree.Class
+import Data.Foldable (Foldable(foldl', foldr'))
+
+import Control.Monad.State.Strict
 
 data HeapT w m a
   = !w :< m (RootF a (HeapT w m a))
@@ -59,17 +62,25 @@ data RootF a b
   = a :> [b]
   deriving (Functor, Foldable, Traversable, Typeable, Generic, Eq, Ord, Show, Read, Generic1, Data)
 
-
 instance Bifunctor RootF where
   bimap f g (x :> y) = f x :> map g y
+  {-# INLINE bimap #-}
   first f (x :> y) = f x :> y
+  {-# INLINE first #-}
   second f (x :> y) = x :> map f y
+  {-# INLINE second #-}
 
 instance Bifoldable RootF where
   bifoldr f g b (x :> xs) = f x (foldr g b xs)
+  bifoldMap f g (x :> xs) = f x <> foldMap g xs
+  bifoldl f g b (x :> xs) = foldl g (f b x) xs
+  {-# INLINE bifoldr #-}
+  {-# INLINE bifoldMap #-}
+  {-# INLINE bifoldl #-}
 
 instance Bitraversable RootF where
   bitraverse f g (x :> xs) = liftA2 (:>) (f x) (traverse g xs)
+  {-# INLINE bitraverse #-}
 
 instance (NFData a, NFData b) => NFData (RootF a b) where
   rnf (x :> xs) = rnf x `seq` rnf xs
@@ -108,11 +119,15 @@ instance (Arbitrary w, Arbitrary a, forall x. Arbitrary x => Arbitrary (m x)) =>
   
 instance Functor m => Functor (HeapT w m) where
   fmap f (w :< xs) = w :< fmap (bimap f (fmap f)) xs
+  {-# INLINE fmap #-}
 
 instance Foldable m => Foldable (HeapT w m) where
   foldr f b (_ :< xs) = foldr (\(y :> ys) z -> f y (foldr (flip (foldr f)) z ys)) b xs
   foldMap f (_ :< xs) = foldMap (\(y :> ys) -> f y <> foldMap (foldMap f) ys) xs
   foldl f b (_ :< xs) = foldl (\z (y :> ys) -> foldl (foldl f) (f z y) ys) b xs
+  
+  foldl' f !b (_ :< xs) = foldl' (\ !z (y :> ys) -> case f z y of !z' -> foldl' (foldl' f) z' ys) b xs
+  foldr' f !b (_ :< xs) = foldr' (\(y :> ys) !z -> f y $! foldr (flip (foldr f)) z ys) b xs
 
 instance Traversable m => Traversable (HeapT w m) where
   traverse f (w :< xs) = fmap (w :<) (traverse (bitraverse f (traverse f)) xs)
@@ -152,9 +167,13 @@ instance (Monoid w, Monad m, Traversable m) => Applicative (HeapT w m) where
   (<*>) = ap
 
 instance (Monoid w, Monad m, Traversable m) => Monad (HeapT w m) where
-  (w1 :< xs) >>= k =
-    let (w2, ys) = traverse (\(x :> xs) -> let w2 :< ys = k x in (w2, fmap (\(z :> zs) -> z :> (zs ++ map (>>= k) xs)) ys)) xs
-    in (w1 <> w2) :< join ys
+  (w1 :< xs) >>= k = w2 :< join ys
+    where
+      (ys, w2) = runState (traverse act xs) w1
+      act (x :> xs) = do
+        let w :< ys = k x
+        modify' (<> w)
+        return (fmap (\(z :> zs) -> z :> (zs ++ map (>>= k) xs)) ys)
   {-# INLINE (>>=) #-}
 
 instance Comonad m => Comonad (HeapT w m) where
